0ff783b5c0209a94f58b2896f2ae523bb8fe5b6d
renthus <renatodasilvamaldonado@gmail.com>
2022-08-01 07:51:46 -0300

    Creation of usuarios.html and navbar.html files

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+user_list = ['Renato', 'Tatiana', 'Valentina']
+
 @app.route("/")
 def home():
     return render_template('home.html')
@@ -9,6 +11,10 @@ def home():
 @app.route("/contato")
 def contato():
     return render_template('contato.html')
+
+@app.route("/usuarios")
+def usuarios():
+    return render_template('usuarios.html', user_list=user_list)
 
 if __name__ == '__main__':
     app.run(debug=True)
