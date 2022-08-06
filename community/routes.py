b43d3525edccb6fc454b8b83eca56afd2b95405e
renthus <renatodasilvamaldonado@gmail.com>
2022-08-06 14:21:20 -0300

    Created a page for only one post

--- community/routes.py
+++ community/routes.py
@@ -114,3 +114,9 @@ def editar_perfil():
         form.username.data = current_user.username
     foto_perfil = url_for('static', filename='fotos_perfil/{}'.format(current_user.foto_perfil))
     return render_template('editarperfil.html', foto_perfil=foto_perfil, form=form)
+
+@app.route('/post/<post_id>')
+@login_required
+def exibir_post(post_id):
+    post = Post.query.get(post_id)
+    return render_template('post.html', post=post)
